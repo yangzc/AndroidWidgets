1950d2c973841890f5478e7107ffb0fbd83c2006
yangzc <yangzc@knowbox.cn>
2017-03-20 11:57:26 +0800

    init commit

---
+++ widgets/src/main/java/com/hyena/widgets/CYImageView.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 The AndroidWidgets Project
+ */
+
+package com.hyena.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by yangzc on 17/2/24.
+ */
+public class CYImageView extends ImageView {
+
+    public CYImageView(Context context) {
+        super(context);
+    }
+
+    public CYImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CYImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+}
---
+++ widgets/src/main/java/com/hyena/widgets/CYTextView.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 The AndroidWidgets Project
+ */
+
+package com.hyena.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by yangzc on 17/2/24.
+ */
+public class CYTextView extends TextView{
+
+    public CYTextView(Context context) {
+        super(context);
+    }
+
+    public CYTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CYTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
